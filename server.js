b9d8dd5572dfcbedd5c0b93c66319265d55d693d
Joao Adherval <joaoadherval@gmail.com>
2021-08-30 18:03:45 -0300

    adding mongodb and mongoose dependecies

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const mongoose = require("mongoose");
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
